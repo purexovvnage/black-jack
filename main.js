4ee8687d6b1776c5646805a9f548b9a7d686a90d
Harrison Truong <harrisontruong@Harrisons-MacBook-Air.local>
2023-01-30 13:05:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,85 @@
+import './style.css'
+
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+
+let cards = [firstCard,secondCard]
+
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = false
+
+let message = ""
+
+const startBtn = document.getElementById("start-btn")
+const newBtn = document.getElementById("new-btn")
+
+startBtn.addEventListener('click',startGame)
+newBtn.addEventListener('click',newCard)
+
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+let player = {
+  name: "Per",
+  chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard,secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function renderGame() {
+
+  let cardmessage  = "Cards: "
+  for(let i = 0; i<cards.length; i++) {
+    cardmessage += cards[i] + " "
+  }
+  cardsEl.textContent = cardmessage
+  sumEl.textContent = "Sum: " + sum
+  if ( sum<21 ) {
+    //console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
+  } else if ( sum === 21 ) {
+    //console.log("Woohoo! You've got Blackjack!")
+    hasBlackJack = true
+    message = "Woohoo! You've got Blackjack!"
+  } else {
+    //console.log("You're out of the game!")
+    isAlive = false
+    message = "You're out of the game!"
+  }
+
+  messageEl.textContent = message
+}
+
+function newCard() {
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
+    cards.push(card)
+    sum += card
+    renderGame()
+  }
+}
+
+function getRandomCard() {
+  let num =  1+Math.floor(Math.random()*13)
+  if (num ===1) {
+    return 11
+  } else if (num > 10) {
+    return 10
+  } else {
+    return num
+  }
+
+
+}
